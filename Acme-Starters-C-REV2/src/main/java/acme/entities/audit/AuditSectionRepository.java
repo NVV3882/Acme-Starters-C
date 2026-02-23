266554d5f9102b6a303f0ab95f1db2ed82616b75
Nicolás Acal <nicacamar@alum.us.es>
2026-02-23 16:01:20 +0100

    Remove commented-out code in AuditSectionRepository

--- Acme-Starters-C-REV2/src/main/java/acme/entities/audit/AuditSectionRepository.java
+++ Acme-Starters-C-REV2/src/main/java/acme/entities/audit/AuditSectionRepository.java
@@ -9,8 +9,6 @@ public interface AuditSectionRepository extends CrudRepository<AuditSection, Int
 	@Query("SELECT SUM(s.hours) FROM AuditSection s WHERE s.report.id = ?1")
 	Integer computeHours(Integer reportId);
 
-	//Computa meses a decimal con funciones de MySQL: la diferencia en segundos entre la media de segundos por mes
-	//@Query(value = "SELECT (UNIX_TIMESTAMP(end_moment) - UNIX_TIMESTAMP(start_moment)) / 2629746.0 FROM audit_report WHERE id = ?1", nativeQuery = true)
-	//Double computeMonthsActive(Integer reportId); //TODO: HACERLO CON MOMENT HELPER
 
 }
+
